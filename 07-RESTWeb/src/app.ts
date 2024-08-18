5e58ce7ebdcbb4d46ae7e7f94d8db6214ec2e0d1
socram174 <socram174@hotmail.com>
2024-08-18 19:52:42 -0400

    SECCION 13: Subir repo a github

--- 07-RESTWeb/src/app.ts
+++ 07-RESTWeb/src/app.ts
@@ -1,49 +1,18 @@
-import fs from 'fs';
-import http2 from 'http2';
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
 
 
-const server = http2.createSecureServer({
-    key: fs.readFileSync('./keys/server.key'),
-    cert: fs.readFileSync('./keys/server.crt'),
-},(req, res)=>{
-
-    console.log(req.url);
 
 
-    // res.writeHead(200,{'Content-Type':'text/html'});
-    // res.write(`<h1 style="color: red;">URL ${req.url}</h1>`); // this is server side rendering
-    // res.end();
+(async ()=>{
+    main();
+})();
 
-    // const data = { name: 'Jhon Dow', age: 30, city: 'New York' };
-    // res.writeHead(200,{'Content-Type':'application/json'});
-    // res.end(JSON.stringify(data));
+function main (){
+    const server = new Server({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+    });
 
-    if(req.url === '/'){
-        const htmlFIle = fs.readFileSync('./public/index.html','utf-8');
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.end(htmlFIle);
-        return;
-    }
-
-    if( req.url?.endsWith('.js')){
-        res.writeHead(200,{'Content-Type':'text/javascript'});
-
-    }else if(req.url?.endsWith('.css')){
-        res.writeHead(200,{'Content-Type':'text/css'});
-    }
-
-    try {
-        const responseContent = fs.readFileSync(`./public/${req.url}`,'utf-8');
-        res.end(responseContent);
-    }catch(err){
-    res.writeHead(404,{'Content-Type':'text/html'});
-    res.end();
-    }
-
-});
-
-server.listen(8080,()=>{
-    console.log('Server is running on port 8080');
-});
-
-//HTTP2
+    server.start();
+}
